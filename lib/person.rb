fd2d668bd28f0a8a4ca25985c6741b9b5c93d1fd
andrew-lewell <andrew.lewell@gmail.com>
2019-10-30 13:15:52 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,3 @@
-# your code goes here
 class Person
 
     attr_reader :name, :happiness, :hygiene
@@ -15,12 +14,18 @@ class Person
         if new_happiness <11 && new_happiness >= 0
             @happiness = new_happiness
         end
+
+        @happiness = 10 if new_happiness >= 10
+        @happiness = 0 if new_happiness <= 0
     end
 
     def hygiene=(new_hygiene)
         if new_hygiene <11 && new_hygiene >= 0
-            @hygiene = new_hygiene
+            return @hygiene = new_hygiene
         end
+
+        @hygiene = 10 if new_hygiene >= 10
+        @hygiene = 0 if new_hygiene <= 0
     end
 
     def clean?
@@ -39,30 +44,31 @@ class Person
     end
 
     def take_bath
-        @hygiene += 4
+        self.hygiene = @hygiene + 4
         "♪ Rub-a-dub just relaxing in the tub ♫"
     end
 
     def work_out
-        @happiness += 2
-        @hygiene -= 3
+        self.happiness = @happiness + 2
+        self.hygiene = (@hygiene - 3)
         "♪ another one bites the dust ♫"
+        
     end
 
     def call_friend(friend)
-        @happiness += 3
+        self.happiness = @happiness + 3
         friend.happiness += 3
         "Hi #{friend.name}! It's #{@name}. How are you?"
     end
 
     def start_conversation(person, topic)
         if topic == "politics"
-            @happiness -= 2
-            person.happiness -= 2
+            self.happiness = @happiness - 2
+            person.happiness = person.happiness - 2
             return "blah blah partisan blah lobbyist"
         elsif topic == "weather" 
-            @happiness += 1
-            person.happiness += 1
+            self.happiness = @happiness + 1
+            person.happiness = person.happiness + 1
             return "blah blah sun blah rain"
         else   
             return "blah blah blah blah blah"
